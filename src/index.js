c024167b36d168a4addcbc3c8e460de7ff46a80f
Di <divasilevski@gmail.com>
2020-06-24 21:28:10 +0300

    Connected empty webpack project

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// Connect styles
+import "./styles/scss.scss"
+
+// Connect libs
+
+import Test from "@models/Test";
+
+Test.sayHello();
+
---
+++ src/models/Test.ts
@@ -0,0 +1,5 @@
+export default class Test {
+  static sayHello(): void {
+    console.log("Hello world!");
+  }
+}
