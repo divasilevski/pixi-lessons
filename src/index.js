499f0d58fc427bb026640b80b3c613c873bdb57c
Di <divasilevski@gmail.com>
2020-06-24 21:43:12 +0300

    connected pixi

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import "./styles/scss.scss"
 // Connect libs
 
 import Test from "@models/Test";
-
 Test.sayHello();
+
+
+////////
+
+let type = "WebGL"
+if (!PIXI.utils.isWebGLSupported()) {
+  type = "canvas"
+}
+
+PIXI.utils.sayHello(type)
+
 
