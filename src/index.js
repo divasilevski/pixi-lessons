192776b08efa78090fed3d94808ce64d008311d2
Di <divasilevski@gmail.com>
2020-07-05 13:26:43 +0300

    Game class on ts

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import "./styles/scss.scss"
 import Test from "./models/Test";
 Test.sayHello();
 
+import Game from "./models/Game";
+
+
 ////////
 import * as PIXI from 'pixi.js';
-import Keyboard from 'pixi.js-keyboard';
-import Mouse from 'pixi.js-mouse';
-import bunnnyImage from "./assets/bunny.png";
 
 let app;
 
@@ -20,66 +20,11 @@ function resize() {
     app.renderer.resize(window.innerWidth, window.innerHeight);
 }
 
-
 app = new PIXI.Application({ backgroundColor: 0x1099bb });
 
 resize()
 
 document.body.appendChild(app.view);
 
-const bunny = PIXI.Sprite.from(bunnnyImage);
-
-bunny.anchor.set(0.5);
-
-bunny.x = app.screen.width / 2;
-bunny.y = app.screen.height / 2;
-
-app.stage.addChild(bunny);
-
-app.ticker.add((delta) => {
-  const speed = 5 * delta;
-
-  if (Keyboard.isKeyDown('ArrowLeft', 'KeyA'))
-    bunny.x -= speed;
-  if (Keyboard.isKeyDown('ArrowRight', 'KeyD'))
-    bunny.x += speed;
-
-  if (Keyboard.isKeyDown('ArrowUp', 'KeyW'))
-    bunny.y -= speed;
-  if (Keyboard.isKeyDown('ArrowDown', 'KeyS'))
-    bunny.y += speed;
-
-  bunny.rotation = getAngleTo(Mouse.getPosX(), Mouse.getPosY(), bunny.x, bunny.y);
-
-  if (Mouse.isButtonDown(Mouse.Button.LEFT)) {
-    bunny.x += getAngleX(speed, bunny.rotation);
-    bunny.y += getAngleY(speed, bunny.rotation);
-  }
-  if (Mouse.isButtonDown(Mouse.Button.RIGHT)) {
-    bunny.x -= getAngleX(speed, bunny.rotation);
-    bunny.y -= getAngleY(speed, bunny.rotation);
-  }
-
-  Keyboard.update();
-  Mouse.update();
-
-
-});
-
-
-function getAngleTo(mx, my, px, py) {
-  var self = this;
-  var distX = my - py;
-  var distY = mx - px;
-  var angle = Math.atan2(distX, distY);
-  //var degrees = angle * 180/ Math.PI;
-  return angle;
-}
-
-function getAngleX(length, angle) {
-  return Math.cos(angle) * length;
-}
-
-function getAngleY(length, angle) {
-  return Math.sin(angle) * length;
-}
+const game = new Game(app);
+game.showBunny();
---
+++ src/models/Game.ts
@@ -0,0 +1,69 @@
+import * as PIXI from 'pixi.js';
+import Keyboard from 'pixi.js-keyboard';
+import Mouse from 'pixi.js-mouse';
+import bunnnyImage from "../assets/bunny.png";
+
+export default class Game {
+  app: PIXI.Application;
+
+  constructor(app: PIXI.Application) {
+    this.app = app;
+  }
+
+  showBunny(): void {
+
+    const bunny = PIXI.Sprite.from(bunnnyImage);
+
+    bunny.anchor.set(0.5);
+
+    bunny.x = this.app.screen.width / 2;
+    bunny.y = this.app.screen.height / 2;
+
+    this.app.stage.addChild(bunny);
+
+    this.app.ticker.add((delta) => {
+      const speed = 5 * delta;
+
+      if (Keyboard.isKeyDown('ArrowLeft', 'KeyA'))
+        bunny.x -= speed;
+      if (Keyboard.isKeyDown('ArrowRight', 'KeyD'))
+        bunny.x += speed;
+
+      if (Keyboard.isKeyDown('ArrowUp', 'KeyW'))
+        bunny.y -= speed;
+      if (Keyboard.isKeyDown('ArrowDown', 'KeyS'))
+        bunny.y += speed;
+
+      bunny.rotation = getAngleTo(Mouse.getPosX(), Mouse.getPosY(), bunny.x, bunny.y);
+
+      if (Mouse.isButtonDown(Mouse.Button.LEFT)) {
+        bunny.x += getAngleX(speed, bunny.rotation);
+        bunny.y += getAngleY(speed, bunny.rotation);
+      }
+      if (Mouse.isButtonDown(Mouse.Button.RIGHT)) {
+        bunny.x -= getAngleX(speed, bunny.rotation);
+        bunny.y -= getAngleY(speed, bunny.rotation);
+      }
+
+      Keyboard.update();
+      Mouse.update();
+    });
+  }
+}
+
+function getAngleTo(mx, my, px, py) {
+  var self = this;
+  var distX = my - py;
+  var distY = mx - px;
+  var angle = Math.atan2(distX, distY);
+  //var degrees = angle * 180/ Math.PI;
+  return angle;
+}
+
+function getAngleX(length, angle) {
+  return Math.cos(angle) * length;
+}
+
+function getAngleY(length, angle) {
+  return Math.sin(angle) * length;
+}
